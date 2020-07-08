03c03b18b24d6e8a9c78625043c6c5d88ce31c78
Julien G <julien.gong@gmail.com>
2020-07-08 18:05:12 +0800

    init
    init

---
+++ _parse.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+try:
+    strFilename = sys.argv[1]
+except:
+    print ("Missing filename.")
+    print ("Usage: python3 _parse.py <input filename> > <output filename>")
+    sys.exit(0)
+
+def getBetween(str, str1, str2):
+    strOutput = str[str.find(str1)+len(str1):str.find(str2)]
+    return strOutput
+
+#strFilename = "Chapter 1 King Fred the Fearless.html"
+#print ("正在处理文件: ", strFilename)
+
+with open(strFilename, 'rt') as objFile:
+    strHTML = objFile.read()
+
+strContentHTML = getBetween(strHTML, '<div class="col">', '<div class="row my-75">')
+strContentHTML = strContentHTML.replace('<h1', '<h2')
+strContentHTML = strContentHTML.replace('</h1>', '</h2>')
+strContentHTML = strContentHTML.replace('<h3>', '<p align="center"><img alt="Crown" src="../Images/Crown.png"/></p><p>')
+strContentHTML = strContentHTML.replace('</h3>', '</p>')
+
+print('<?xml version="1.0" encoding="utf-8"?>')
+print('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"')
+print('  "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">')
+print('')
+print('<html xmlns="http://www.w3.org/1999/xhtml">')
+print('<head>')
+print('  <title></title>')
+print('</head>')
+print('')
+print('<body>')
+print ("<div><div>", strContentHTML)
+print('</body>')
+print('</html>')
